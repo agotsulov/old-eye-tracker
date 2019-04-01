eddbf1c61cb9ea3d38b328702fd742c9d64d4083
Agotsulov <agotsulov@gmail.com>
2019-04-01 19:01:47 +0600

    add requirements.txt

--- start.py
+++ start.py
@@ -16,7 +16,8 @@ pygame.init()
 camera_port = 0
 camera = cv2.VideoCapture(camera_port)
 
-pretrained_face_landmark = "shape_predictor_68_face_landmarks.dat" #http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
+pretrained_face_landmark = "shape_predictor_68_face_landmarks.dat"
+# http://dlib.net/files/shape_predictor_68_face_landmarks.dat.bz2
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(pretrained_face_landmark)
 
@@ -24,7 +25,8 @@ inst = pygame.image.load('instruction.jpg')
 
 infoObject = pygame.display.Info()
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-screen = pygame.display.set_mode((0, 0)) #Костыли так как фуллскрине нельзя alt+tab ,но и открыть окно на весь экрано просто нельзя там рамка(
+screen = pygame.display.set_mode((0, 0))
+# Костыли так как фуллскрине нельзя alt+tab ,но и открыть окно на весь экрано просто нельзя там рамка(
 time.sleep(0.1)  
 
 max_w, max_h = pygame.display.get_surface().get_size()
@@ -35,8 +37,8 @@ quit = False
 dir = "data"
 last_image = ''
 
-x = random.randint(0, max_w) #infoObject.current_w   
-y = random.randint(0, max_h) #infoObject.current_h   
+x = random.randint(0, max_w) # infoObject.current_w
+y = random.randint(0, max_h) # infoObject.current_h
 
 left_eye = [100, 100]
 right_eye = [300, 300]
@@ -44,7 +46,7 @@ right_eye = [300, 300]
 if not os.path.exists(dir):
     os.makedirs(dir)
 
-while quit == False:
+while not quit:
     time.sleep(1 / 30)  
 
     return_value, frame = camera.read()
@@ -64,7 +66,7 @@ while quit == False:
     for (i, rect) in enumerate(rects):
         shape = predictor(frame, rect)
         shape = face_utils.shape_to_np(shape)
-        for (x_, y_) in shape: #36 42 43 48
+        for (x_, y_) in shape: # 36 42 43 48
             pygame.draw.circle(screen, (0, 255, 0), (x_, y_), 2)
 
 
@@ -93,5 +95,5 @@ while quit == False:
             quit = True
            
 
-#cam.stop()
+# cam.stop()
 pygame.quit()
