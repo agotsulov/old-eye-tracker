fedbf23edeaf606a377d52c512649680985a2231
Agotsulov <agotsulov@gmail.com>
2019-04-02 18:17:04 +0600

    add not worked train

--- start.py
+++ start.py
@@ -24,8 +24,8 @@ predictor = dlib.shape_predictor(pretrained_face_landmark)
 inst = pygame.image.load('instruction.jpg')
 
 infoObject = pygame.display.Info()
+print(infoObject)
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-screen = pygame.display.set_mode((0, 0))
 # Костыли так как фуллскрине нельзя alt+tab ,но и открыть окно на весь экрано просто нельзя там рамка(
 time.sleep(0.1)  
 
@@ -37,45 +37,54 @@ quit = False
 dir = "data"
 last_image = ''
 
-x = random.randint(0, max_w) # infoObject.current_w
-y = random.randint(0, max_h) # infoObject.current_h
+x = random.randint(0, max_w)  # infoObject.current_w
+y = random.randint(0, max_h)  # infoObject.current_h
 
-left_eye = [100, 100]
-right_eye = [300, 300]
+needW = 200
+needH = 100
 
 if not os.path.exists(dir):
     os.makedirs(dir)
 
+eyes = None
+shape = None
+
 while not quit:
-    time.sleep(1 / 30)  
+    time.sleep(1 / 30)
+
+    eyes = None
+    shape = None
 
     return_value, frame = camera.read()
-    
+
     frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2RGB)
 
-    img = pygame.image.frombuffer(frame.tostring(), frame.shape[1::-1], "RGB") 
+    img = pygame.image.frombuffer(frame.tostring(), frame.shape[1::-1], "RGB")
 
     screen.fill((255, 255, 255))
-    screen.blit(img, (0,0))
-    screen.blit(inst, (600,0))
+    screen.blit(img, (0, 0))
+    screen.blit(inst, (600, 0))
 
     pygame.draw.circle(screen, (255, 0, 0), (x, y), 8)
-    
+
     rects = detector(frame, 0)
-    
+
     for (i, rect) in enumerate(rects):
         shape = predictor(frame, rect)
         shape = face_utils.shape_to_np(shape)
-        for (x_, y_) in shape: # 36 42 43 48
-            pygame.draw.circle(screen, (0, 255, 0), (x_, y_), 2)
 
+        for (x_, y_) in shape:  # 36 42 43 48
+            pygame.draw.circle(screen, (0, 255, 0), (x_, y_), 2)
 
         (x_, y_, w_, h_) = cv2.boundingRect(np.array([shape[36:48]]))
         eyes = frame[y_ - 10:y_ + h_ + 10, x_ - 10:x_ + w_ + 10]
-        eyes_frame = pygame.image.frombuffer(eyes.tostring(), (eyes.shape[1], eyes.shape[0]) , "RGB") 
-            
-        screen.blit(eyes_frame, (0  ,frame.shape[0]))
-        
+
+        eyes = cv2.resize(eyes, (64, 32))
+
+        eyes_frame = pygame.image.frombuffer(eyes.tostring(), (eyes.shape[1], eyes.shape[0]), "RGB")
+
+        screen.blit(eyes_frame, (0, frame.shape[0]))
+
     pygame.display.flip()
 
     events = pygame.event.get()
@@ -83,9 +92,9 @@ while not quit:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 quit = True
-            if event.key == pygame.K_SPACE:        
+            if event.key == pygame.K_SPACE and eyes is not None:
                 last_image = dir + "/{}_x_{}_y_{}.jpg".format(int(time.time()), x, y)
-                pygame.image.save(img, last_image) 
+                pygame.image.save(img, last_image)
                 x = random.randint(0, max_w)
                 y = random.randint(0, max_h)
             if event.key == pygame.K_z:
@@ -93,7 +102,6 @@ while not quit:
                     os.remove(last_image)
         if event.type == pygame.QUIT:
             quit = True
-           
 
 # cam.stop()
 pygame.quit()
---
+++ train.py
@@ -0,0 +1,168 @@
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+
+from imutils import face_utils
+import dlib
+import cv2
+import numpy as np
+import os
+from scipy import misc
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self):
+        self.len = len(os.listdir('data'))
+        self.pretrained_face_landmark = "shape_predictor_68_face_landmarks.dat"
+        self.detector = dlib.get_frontal_face_detector()
+        self.predictor = dlib.shape_predictor(self.pretrained_face_landmark)
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, index):
+        names = os.listdir('data')
+        curr = names[index]
+
+        frame = misc.imread('data/' + curr)
+
+        rects = self.detector(frame, 0)
+
+        eyes = None
+        face = None
+
+        for (i, rect) in enumerate(rects):
+            shape = self.predictor(frame, rect)
+            shape = face_utils.shape_to_np(shape)
+
+            (x_, y_, w_, h_) = cv2.boundingRect(np.array([shape[36:48]]))
+            eyes = frame[y_ - 10:y_ + h_ + 10, x_ - 10:x_ + w_ + 10]
+
+            eyes = cv2.resize(eyes, (64, 32))
+
+            face = shape
+
+        x = int(curr.split('_')[2]) / 1920
+        y = int(curr[:-4:].split('_')[4]) / 1080
+
+        # Иногда проскакивают картиники на которых dlib не может найти лицо второй раз
+        print(curr)
+        print(type(eyes))
+        print(type(face))
+        print(eyes.shape)
+
+        return torch.from_numpy(eyes), torch.from_numpy(face), torch.from_numpy(np.array([x, y]))
+
+
+
+
+# Device configuration
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+# Hyper parameters
+num_epochs = 5
+num_classes = 10
+batch_size = 100
+learning_rate = 0.001
+'''
+# MNIST dataset
+train_dataset = torchvision.datasets.MNIST(root='../../data/',
+                                           train=True,
+                                           transform=transforms.ToTensor(),
+                                           download=True)
+
+test_dataset = torchvision.datasets.MNIST(root='../../data/',
+                                          train=False,
+                                          transform=transforms.ToTensor())
+
+# Data loader
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=False)
+'''
+
+train_dataset = Dataset()
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True)
+
+class Flatten(nn.Module):
+    def forward(self, x):
+        return x.reshape(x.size(0), -1)
+
+
+class ConvNet(nn.Module):
+    def __init__(self, num_classes=2):
+        super(ConvNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, padding=1)
+        self.maxpool1 = nn.MaxPool2d(2)
+        self.fc = nn.Linear(32 * 16 * 64 + 68 * 2, num_classes)
+
+    def forward(self, e, f):
+        print("FORWARD")
+        print(e)
+        print(f)
+        print(e.size())
+        print(f.size())
+        out = F.relu(self.conv1(e))
+        out = self.maxpool1(out)
+        out = out.reshape(out.size(0), -1)
+        f = f.reshape(f.size(0), -1)
+        out = torch.cat(out, f)
+        out = self.fc(out)
+        return out
+
+
+model = ConvNet(num_classes).to(device)
+
+# Loss and optimizer
+criterion = torch.nn.MSELoss(reduction='sum')
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Train the model
+total_step = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (eyes, face, pos) in enumerate(train_loader):
+        eyes = eyes.to(device)
+        face = face.to(device)
+        pos = pos.to(device)
+
+        # Forward pass
+        outputs = model(eyes, face)
+        loss = criterion(outputs, pos)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i + 1) % 100 == 0:
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
+                  .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
+
+torch.save(model.state_dict(), 'model.ckpt')
+
+'''
+# Test the model
+model.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
+with torch.no_grad():
+    correct = 0
+    total = 0
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
+'''
+# Save the model checkpoint
+torch.save(model.state_dict(), 'model.ckpt')
